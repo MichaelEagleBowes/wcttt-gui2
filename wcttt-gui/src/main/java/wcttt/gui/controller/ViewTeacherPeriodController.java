2bb22cbfc47d537e75215f30e5d8374ca6c9dcfd
MichaelEagleBowes <michael.bowes@stud.uni-bamberg.de>
2018-10-31 17:06:37 +0100

    Added MatrixViewControllers.

---
+++ wcttt-gui/src/main/java/wcttt/gui/controller/ViewTeacherPeriodController.java
@@ -0,0 +1,18 @@
+package wcttt.gui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class ViewTeacherPeriodController extends Controller {
+
+	@FXML
+	private TextField xAxisName;
+	
+	@FXML
+	private TextField yAxisName;
+	
+	@FXML
+	private GridPane matrix;
+	
+}
