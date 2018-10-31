272c484469050858395c09ea8351bb8da9caaa91
MichaelEagleBowes <michael.bowes@stud.uni-bamberg.de>
2018-10-31 21:19:09 +0100

    added generate Matrix button

--- wcttt-gui/src/main/java/wcttt/gui/controller/ViewTeacherPeriodController.java
+++ wcttt-gui/src/main/java/wcttt/gui/controller/ViewTeacherPeriodController.java
@@ -1,17 +1,26 @@
 package wcttt.gui.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import wcttt.gui.WctttGuiException;
 import wcttt.lib.model.Curriculum;
 import wcttt.lib.model.Timetable;
+import wcttt.lib.model.WctttModelException;
 
 public class ViewTeacherPeriodController extends Controller {
 
 	private MainController mainController;
+	
+	@FXML
+	private Button genButton;
 	
 	@FXML
 	private BorderPane rootPane;
@@ -36,12 +45,17 @@ public class ViewTeacherPeriodController extends Controller {
 		Label label3 = new Label("C");
 		matrix.add(label1, 0, 0, 2, 1);
 		matrix.add(label2, 0, 1, 1, 1);
+		genButton.setOnAction(event -> {
+			generateMatrix();
+		});
 		
-		Timetable table;
-		getModel().getSemester();
 		//table = list.get(0);
 		//table = getMainController().getTimetableTableController().getSelectedTimetable();
 		matrix.add(label3, 1, 1, 1, 1);
+	}
+	
+	private void generateMatrix() {
+		Util.informationAlert("Model", "The Matrix for " + getModel().getName() + " was generated!");
 	}
 	
 	private void addRows() {
